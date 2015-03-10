2456d1325169949547bb319ebacc5aa225c4d012
semoro <sem-oro@yandex.ru>
2015-03-10 18:53:34 +0300

    Танцы с бубном

--- configuration.h
+++
@@ -1,7 +0,0 @@
-#include <QString>
-#ifndef CONFIGURATION_H
-#define CONFIGURATION_H
-#define USE_QXT
-#define API_URL QString("http://sescreen.ddns.net/api")
-#define WEB_URL QString("http://sescreen.ddns.net")
-#endif // CONFIGURATION_H
